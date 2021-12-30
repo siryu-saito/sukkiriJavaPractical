5cffcda63f52d106de903a777cc02cfd4004f6c3
siryu-saito <saito.s.ryu@gmail.com>
2021-12-30 15:58:06 +0900

    実践編 3-4 練習問題

--- lesson3/src/lesson3/Main.java
+++ lesson3/src/lesson3/Main.java
@@ -1,14 +1,13 @@
 package lesson3;
-import java.util.function.IntPredicate;
+import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
-		IntPredicate f1 = x -> x % 2 == 1;
-		Func2 f2 = (point, name) -> {
-			return name + "さんは" + ( point > 65 ? "合格" : "不合格" );
-		};
-		
-		System.out.println(f1.test(15));
-		System.out.println(f2.call(88, "misaki"));
+		List<String> names =
+			List.of("豊臣秀吉", "島左近", "徳川家康");
+		names.stream()
+			.filter(n -> n.length() <= 4)
+			.map(n -> n + "さん")
+			.forEach(System.out::println);
 	}
 }
