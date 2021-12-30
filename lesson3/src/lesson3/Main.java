c2ee6070ce0cb07a995264d9ed517574bc467ee4
siryu-saito <saito.s.ryu@gmail.com>
2021-12-30 15:43:52 +0900

    実践編 3-3 練習問題

--- lesson3/src/lesson3/Main.java
+++ lesson3/src/lesson3/Main.java
@@ -1,13 +1,14 @@
 package lesson3;
+import java.util.function.IntPredicate;
 
 public class Main {
 	public static void main(String[] args) {
-		Func1 f1 = x -> x % 2 == 1;
+		IntPredicate f1 = x -> x % 2 == 1;
 		Func2 f2 = (point, name) -> {
 			return name + "さんは" + ( point > 65 ? "合格" : "不合格" );
 		};
 		
-		System.out.println(f1.call(15));
+		System.out.println(f1.test(15));
 		System.out.println(f2.call(88, "misaki"));
 	}
 }
